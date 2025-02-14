d0cdbf04b0421eda193645f99fb239dd9575b30a
TanyaGrek <3334847@gmail.com>
2025-02-13 19:37:28 -0500

    task-2

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,47 @@
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create S3 Bucket for hosting
+    const siteBucket = new s3.Bucket(this, "MyShopBucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // Create CloudFront Distribution
+    const distribution = new cloudfront.Distribution(this, "Shop-distribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED
+      },
+      defaultRootObject: "index.html",
+      errorResponses: [{
+        httpStatus: 404,
+        responseHttpStatus: 200,
+        responsePagePath: "/index.html"
+      }]
+    });
+
+    // Deploy frontend app to S3
+    new s3deploy.BucketDeployment(this, "Shop-bucket-Deployment", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: siteBucket,
+      distribution: distribution,
+      distributionPaths: ["/*"]
+    });
+
+    // Output CloudFront URL
+    new cdk.CfnOutput(this, "CloudFrontURL", {
+      value: distribution.distributionDomainName
+    });
+  }
+}
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,11 @@
+const REGION = "us-east-2";
+
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: `https://.execute-api.${REGION}.amazonaws.com/dev`,
+  order: `https://.execute-api.${REGION}.amazonaws.com/dev`,
+  import: `https://.execute-api.${REGION}.amazonaws.com/dev`,
+  bff: `https://.execute-api.${REGION}.amazonaws.com/dev`,
+  cart: `https://.execute-api.${REGION}.amazonaws.com/dev`,
 };
 
 export default API_PATHS;
